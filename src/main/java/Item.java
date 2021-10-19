3f4d5256fa06f3306a98162d9c9a172471d85454
Ashish <ashish_mohanty10@yahoo.com>
2021-10-19 16:01:31 +0530

    Part 3 : Solution

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -2,9 +2,7 @@ public class Item {
     private String name;
     private int price;
 
-    public int getPrice() {
-        return price;
-    }
+   
 
     public Item(String name, int price) {
         this.name = name;
